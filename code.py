75b02b2642067dc15b457686c3d3b2062c7e45be
Kate Xu <38595657+k8xu@users.noreply.github.com>
2020-08-07 16:11:57 -0400

    Add hello function

--- code.py
+++ code.py
@@ -12,3 +12,10 @@ def multiply(a ,
 b):
     product  = a   *  b
     return product
+
+def say_hello(name  ):
+    """
+    This is a function that takes in an input name and prints hello and the input name back to the user. The name must be a string.
+    """
+    output   = f"Hello {name}!"
+    print(output)
