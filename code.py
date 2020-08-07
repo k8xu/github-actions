97d38e11787af812839927f4ab7554b9ed9c820c
Kate Xu <38595657+k8xu@users.noreply.github.com>
2020-08-07 15:49:52 -0400

    Add example code

---
+++ code.py
@@ -0,0 +1,10 @@
+# Example (bad) Python code to observe Black and flake8 in action!
+
+def add(a,    b):
+    sum  =   a  +  b
+    return sum
+
+def multiply(a ,
+b):
+    product  = a   *  b
+    return product
